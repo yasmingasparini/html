4148b7616a0ee4aac8b39a532a0ad220b81d7750
yasmingasparini <yasmingl1108@gmail.com>
2025-06-16 10:43:36 -0300

    Add files via upload
    16/06/2025.

---
+++ yasmin.js
@@ -0,0 +1,11 @@
+/*
+Desenvolver um programa que leia uma temperatura em graus Celsius e apresenta-la convertida em graus Fahrenheit.
+formula:
+F = C*9/5+32, sendo F a temperatura em Fahrenheit e C a temperatura em C em Celsius
+*/
+
+temperatura = prompt ("digite uma temperatura")
+F = temperatura * 9/ 5 + 32
+alert(" Fahrenheit = celsius * 9/5 +32 graus é: " + F)
+
+
